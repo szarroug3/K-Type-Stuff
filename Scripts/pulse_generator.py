64e8be6b0a4b43bba0a451219a1de7f9badac176
Samreen Zarroug <szarroug3@gmail.com>
2018-03-21 19:12:22 -0500

    updating pulse_generator

--- Scripts/pulse_generator.py
+++ Scripts/pulse_generator.py
@@ -1,101 +1,95 @@
+import os
 import json
 import argparse
 from copy import copy
 from random import randint
+try:
+    from itertools import izip_longest
+    zip_longest = izip_longest
+except:
+    from itertools import zip_longest
 
 
 data = {
-            "animations": {
-                            "pulse_static": {
-                                                "settings": "loops:1, framedelay:2, pfunc:interp,start"
-                                            },
-                            "pulse_changer": {
-                                                "settings": "framedelay:5, loop, pfunc:interp,start"
-                                             }
-                          }
-        }
+            "pulse_static": {"settings": "loops:1, framedelay:2, pfunc:interp,start"},
+            "pulse_changer": {"settings": "framedelay:5, loop, pfunc:interp,start"}
+       }
 
 def get_delta(start, end, color):
     if start[color] == end[color]:
         return 0
     return 1
 
-def create_frames(start, end, deltas):
-    for key in ['R', 'G', 'B']:
-        if start[key] == end[key]:
-            deltas[key] = 0
-            continue
-        if end[key] > start[key]:
-            while deltas[key] > 0 and start[key] + deltas[key] > end[key]:
-                deltas[key] -= 1
-            start[key] += deltas[key]
-            deltas[key + 'S'] = '+'
-        else:
-            while deltas[key] > 0 and start[key] - deltas[key] < end[key]:
-                deltas[key] -= 1
-            start[key] -= deltas[key]
-            deltas[key + 'S'] = '-'
-    return deltas, start
-
-def equal_colors(color_a, color_b):
-    for color in ['R', 'G', 'B']:
-        if color_a[color] != color_b[color]:
-            return False
-    return  True
-
-def switch_signs(data):
-    for key in ['RS', 'GS', 'BS']:
-        if data[key] == '+':
-            data[key] = '-'
-        else:
-            data[key] = '+'
-    return data
-
-def pulse_writer(color_a, color_b, deltas):
-    color_a.update({'RS': '+', 'GS': '+', 'BS': '+'})
-    rgb_line = 'P[c:-10%]({RS}:{R},{GS}:{G},{BS}:{B}), P[c:110%]({RS}:{R},{GS}:{G},{BS}:{B})'
+def create_frames(start, end, color_deltas):
     frames = []
-    backwards_frames = []
-    static_frames = [rgb_line.format(**color_a)]
-    while not equal_colors(color_a, color_b):
-        deltas, color_a = create_frames(color_a, color_b, deltas)
-        frames.append(rgb_line.format(**deltas))
-        backwards_frames.insert(0, rgb_line.format(**switch_signs(deltas)))
-    return static_frames, frames + backwards_frames
+    reverse_frames = []
+    current = copy(start)
+    deltas = copy(color_deltas)
+    while current != end:
+        frame = {'R': 0, 'G': 0, 'B': 0, 'RP': True, 'GP': True, 'BP': True}
+        reverse = {'R': 0, 'G': 0, 'B': 0, 'RP': False, 'GP': False, 'BP': False}
+        for key in ['R', 'G', 'B']:
+            frame[key + 'C'] = current[key]
+            if current[key] == end[key]:
+                deltas[key] = 0
+                frame[key + 'P'] = True
+                reverse[key + 'P'] = False
+            elif current[key] < end[key]:
+                while deltas[key] > 0 and current[key] + deltas[key] > end[key]:
+                    deltas[key] -= 1
+                current[key] += deltas[key]
+                frame[key + 'P'] = True
+                reverse[key + 'P'] = False
+            else:
+                while deltas[key] > 0 and current[key] - deltas[key] < end[key]:
+                    deltas[key] -= 1
+                current[key] -= deltas[key]
+                frame[key + 'P'] = False
+                reverse[key + 'P'] = True
+            frame[key] = deltas[key]
+            reverse[key] = deltas[key]
+            reverse[key + 'C'] = current[key]
+        frames.append(frame)
+        reverse_frames.insert(0, reverse)
+    return frames + reverse_frames
 
-def pulse_writer_random(color_a, color_b, deltas):
-    r_range = sorted([color_a['R'], color_b['R']])
-    g_range = sorted([color_a['G'], color_b['G']])
-    b_range = sorted([color_a['B'], color_b['B']])
+def write_frames(frames, key=None):
+    if key:
+        static = 'P[{K}](+:{{RC}},+:{{GC}},+:{{BC}})'.format(K=key)
+        pulse = 'P[{K}]({{RS}}:{{R}},{{GS}}:{{G}},{{BS}}:{{B}})'.format(K=key)
+    else:
+        static = 'P[c:-10%](+:{RC},+:{GC},+:{BC}), P[c:110%](+:{RC},+:{GC},+:{BC})'
+        pulse = 'P[c:-10%]({RS}:{R},{GS}:{G},{BS}:{B}), P[c:110%]({RS}:{R},{GS}:{G},{BS}:{B})'
+    return static.format(**frames[0]), [pulse.format(RS='+' if frame['RP'] else '-', GS='+' if frame['GP'] else '-', BS='+' if frame['BP'] else '-', **frame) for frame in frames]
 
-    rgb_line = 'P[{K}]({RS}:{R},{GS}:{G},{BS}:{B})'
+def create_pulse(start, end, color_deltas, random=False):
+    frames = create_frames(start, end, color_deltas)
 
-    frames = []
-    backwards_frames = []
+    if not random:
+        static, pulse = write_frames(frames)
+        return [static], pulse
+
+    random_frames = {}
     static_frames = []
+    for k in range(1, 120):
+        start = randint(0, len(frames) - 1)
+        if k == 1:
+            check(frames[start:] + frames[:start])
+        static, pulse = write_frames(frames[start:] + frames[:start], key=k)
+        static_frames.append(static)
+        random_frames[k] = pulse
+    return static_frames, [','.join(filter(None, frame)) for frame in zip_longest(*random_frames.values(), fillvalue='')]
 
-    for i in range(1, 120):
-        start = {'R': randint(*r_range), 'G': randint(*g_range), 'B': randint(*b_range),
-                 'RS': '+', 'GS': '+', 'BS': '+', 'K': i}
-        deltas['K'] = i
-        current = copy(start)
-        current_deltas = copy(deltas)
-        static_frames.append(rgb_line.format(**start))
-        while not equal_colors(current, color_b):
-            current_deltas, current = create_frames(current, color_b, current_deltas)
-            frames.append(rgb_line.format(**current_deltas))
-            backwards_frames.insert(0, rgb_line.format(**switch_signs(current_deltas)))
-        frames += backwards_frames
-
-        current = copy(start)
-        current_deltas = copy(deltas)
-        backwards_frames = []
-        while not equal_colors(current, color_a):
-            current_deltas, current = create_frames(current, color_a, current_deltas)
-            frames.append(rgb_line.format(**current_deltas))
-            backwards_frames.insert(0, rgb_line.format(**switch_signs(current_deltas)))
-        frames += backwards_frames
-    return static_frames, frames
+def check(frames):
+    curr = {'R': frames[0]['RC'], 'G': frames[0]['GC'], 'B': frames[0]['BC']}
+    print(curr['R'], curr['G'], curr['B'])
+    for f in frames:
+        for k in curr.keys():
+            if f[k + 'P']:
+                curr[k] += f[k]
+            else:
+                curr[k] -= f[k]
+        print(curr['R'], curr['G'], curr['B'])
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Create a pulse rgb animation for the K-Type')
@@ -114,20 +108,12 @@ if __name__ == '__main__':
 
     deltas = {'R': get_delta(color_a, color_b, 'R') if not args.r_delta else abs(args.r_delta),
               'G': get_delta(color_a, color_b, 'G') if not args.g_delta else abs(args.g_delta),
-              'B': get_delta(color_a, color_b, 'B') if not args.b_delta else abs(args.b_delta),
-              'RS': '+', 'GS': '+', 'BS': '+'}
+              'B': get_delta(color_a, color_b, 'B') if not args.b_delta else abs(args.b_delta)}
 
-    if args.random:
-        static, pulse = pulse_writer_random(color_a, color_b, deltas)
-    else:
-        static, pulse = pulse_writer(color_a, color_b, deltas)
+    static, pulse = create_pulse(color_a, color_b, deltas, random=args.random)
 
-    data['animations']['pulse_static']['frames'] = static
-    data['animations']['pulse_changer']['frames'] = pulse
-    with open('animation.json', 'w') as file:
+    data['pulse_static']['frames'] = static
+    data['pulse_changer']['frames'] = pulse
+    filename = 'pulse_animation_{R}_{G}_{B}__{{R}}_{{G}}_{{B}}.json'.format(**color_a).format(**color_b)
+    with open(os.path.join(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'Animations'), filename), 'w') as file:
         json.dump(data, file, indent=4)
-
-
-
-# 236, 59, 154 -- pink
-# 34, 160, 255 -- blue
